541843e8b658af5854b232fdae6c8903d3f616ab
Hendy Racher <git@hendyracher.co.uk>
2018-06-16 07:02:54 +0100

    property editor functional

--- src/Our.Umbraco.ResizingTextarea/Resources/ResizingTextarea.js
+++ src/Our.Umbraco.ResizingTextarea/Resources/ResizingTextarea.js
@@ -9,9 +9,38 @@
     ResizingTextareaController.$inject = ['$scope'];
 
     function ResizingTextareaController($scope) {
+    };
 
-        $scope.model.value = $scope.model.value || '';
 
-    }
+    // https://hassantariqblog.wordpress.com/2016/07/27/angularjs-textarea-auto-resize-directive/
+    angular
+        .module("umbraco.directives")
+        .directive('autoResize', autoResize);
+
+    autoResize.$inject = ['$timeout'];
+
+    function autoResize($timeout) {
+
+        var directive = {
+            restrict: 'A',
+            link: function autoResizeLink(scope, element, attributes, controller) {
+
+                element.css({ 'height': 'auto', 'overflow-y': 'hidden' });
+
+                $timeout(function () {
+                    element.css('height', element[0].scrollHeight + 'px');
+                }, 100);
+
+                element.on('input', function () {
+                    element.css({ 'height': 'auto', 'overflow-y': 'hidden' });
+                    element.css('height', element[0].scrollHeight + 'px');
+
+                });
+            }
+        };
+
+        return directive;
+    };
+
 
 })();
