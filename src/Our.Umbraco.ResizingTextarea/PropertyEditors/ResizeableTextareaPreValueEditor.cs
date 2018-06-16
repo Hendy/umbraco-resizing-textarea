e2bac493bc2968c09bd46c6c39384e37e6d2af87
Hendy Racher <git@hendyracher.co.uk>
2018-06-16 05:53:20 +0100

    added property editor defination class

---
+++ src/Our.Umbraco.ResizingTextarea/PropertyEditors/ResizeableTextareaPreValueEditor.cs
@@ -0,0 +1,8 @@
+﻿using Umbraco.Core.PropertyEditors;
+
+namespace Our.Umbraco.ResizingTextarea.PropertyEditors
+{
+    internal class ResizeableTextareaPreValueEditor : PreValueEditor
+    {
+    }
+}
