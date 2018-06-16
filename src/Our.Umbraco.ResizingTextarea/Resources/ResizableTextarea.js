a3a3a3a9d0dae5a4d02ecd7a7e769e7293d9f1c0
Hendy Racher <git@hendyracher.co.uk>
2018-06-16 05:43:47 +0100

    added embedded resources

---
+++ src/Our.Umbraco.ResizingTextarea/Resources/ResizableTextarea.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    'use strict';
+
+    angular
+        .module("umbraco")
+        .controller("ResizeableTextareaController", ResizeableTextareaController);
+
+    ResizeableTextareaController.$inject = ['$scope'];
+
+    function ResizeableTextareaController($scope) {
+
+        $scope.model.value = $scope.model.value || '';
+
+    }
+
+})();
